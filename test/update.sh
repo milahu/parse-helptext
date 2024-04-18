fc2ed7ea80f0db86fbc655e8aa832ddf52b10624
Milan Hauth <milahu@gmail.com>
2024-04-18 22:51:10 +0200

    up test/update.sh

--- test/update.sh
+++ test/update.sh
@@ -3,12 +3,14 @@
 set -e # stop on error
 #set -x # xtrace
 
+parse_helptext=./parse_helptext.py
+
 cd "$(dirname "$0")/.."
 
 for txt in test/cases/*.txt; do
   echo "txt: $txt"
   base=${txt%.*}
-  cat $txt | ./help2comp.py --json >$base.json
-  cat $txt | ./help2comp.py --gen-parse-sh >$base.sh
+  cat $txt | $parse_helptext --json >$base.json
+  cat $txt | $parse_helptext --gen-argparse-sh >$base.sh
   chmod +x $base.sh
 done
